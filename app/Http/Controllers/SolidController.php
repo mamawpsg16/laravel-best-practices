8be5c4500a81737ba65ae00e47be39baf486e540
Kevin Mensah <48464499+mamawpsg16@users.noreply.github.com>
2024-07-02 10:05:44 +0800

    For Testing Purposes

--- app/Http/Controllers/SolidController.php
+++ app/Http/Controllers/SolidController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Test\Solid\OCP\Services\CircleService;
 use App\Test\Solid\OCP\Services\AreaCalculator;
-use App\Test\Solid\OCP\Services\Interfaces\PaymentGatewayInterface;
 use App\Test\Solid\OCP\Services\RectangleService;
 use App\Test\Solid\SRP\Services\Export\CsvExportService;
 use App\Test\Solid\SRP\Services\Export\PdfExportService;
+use App\Test\Solid\OCP\Services\Interfaces\PaymentGatewayInterface;
 
 class SolidController extends Controller
 {
@@ -26,6 +27,37 @@ class SolidController extends Controller
     }
     public function index(Request $request)
     {
+
+        // $fields = "name, email, banned_at";
+        // $where = "WHERE 1=1";
+        // $name = 'Kevin';
+        // // Variable for name filter
+        // $name = 'Kevin';
+
+        // // Constructing the query
+        // $where = " WHERE 1=1";
+        // // $where .= " AND name LIKE '%".$name."%'";
+        // $where .= " AND id  = 66";
+        // // Final SQL query
+        // $sql = "SELECT $fields FROM users" . $where. " LIMIT 1";
+        // // dd($sql);
+        // $abc = DB::SELECT($sql);
+        // dd($abc);
+
+        $emails = "kevinmensah110@gmail.com,qweqwe@gmail.com";
+        $exploded_emails = explode(',', str_replace(' ', '', $emails));
+        $additionalCondition ="";
+        
+        foreach ($exploded_emails as $key => $email) {
+            $additionalCondition .= $key == 0 ? " WHERE FIND_IN_SET('$email',email) " : " OR FIND_IN_SET('$email',email)";
+        }
+        // $params = implode(',',array_map(function($ref) {
+        //     return "'".$ref."'";
+        // }, $exploded_emails));
+        // $condition = " WHERE email IN ($params)";
+        $query = "SELECT * FROM users".$additionalCondition;
+        $result =   DB::SELECT($query);
+        dd($result);
         /** OCP */
     //    $shapes = [new CircleService(10), new RectangleService(10,10)];
     //    $area  = $this->areaCalculator->totalArea(new CircleService(10), new RectangleService(10,10));
