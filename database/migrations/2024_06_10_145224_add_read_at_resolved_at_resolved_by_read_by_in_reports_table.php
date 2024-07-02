da9dd95f3d31d15a663dcfd6ba2fa017b4427d74
Kevin Mensah <48464499+mamawpsg16@users.noreply.github.com>
2024-07-02 10:05:18 +0800

    fix(add columns in reports table)
    - Remove resolved status in migration

--- database/migrations/2024_06_10_145224_add_read_at_resolved_at_resolved_by_read_by_in_reports_table.php
+++ database/migrations/2024_06_10_145224_add_read_at_resolved_at_resolved_by_read_by_in_reports_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->timestamp('read_at')->after('description')->nullable();
             $table->string('read_by',100)->after('read_at')->nullable();
             $table->timestamp('resolved_at')->after('read_by')->nullable();
-            $table->integer('resolved_status')->after('resolved_at')->default(0)->comment('0 - Pending, 1 - Under Investigation, 2 - Resolved');
+            // $table->integer('resolved_status')->after('resolved_at')->default(0)->comment('0 - Pending, 1 - Under Investigation, 2 - Resolved');
             $table->string('resolved_by',100)->after('resolved_at')->nullable();
         });
     }
